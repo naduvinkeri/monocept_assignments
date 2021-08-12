ee0ad141a6325fed5854b1d27e13f0e646708b29
naduvinkeri <naduvinkeri5@gmail.com>
2021-08-12 13:39:53 +0530

    code submission

---
+++ code_refactoring8/Mamburger.java
@@ -0,0 +1,16 @@
+public class Hamburger extends Sandwich{
+	
+	void addMeat() {
+		
+		System.out.println("Hamburger Added");
+		
+	}
+
+
+	void addCondiments() {
+		
+		System.out.println("Special Sauce Added");
+		
+	}
+
+}
---
+++ code_refactoring8/veggiesub.java
@@ -0,0 +1,16 @@
+public class VeggieSub extends Sandwich{
+	
+	boolean customerWantsMeat(){ return false; }
+
+	void addMeat() {
+		
+	}
+
+
+	void addCondiments() {
+		
+		System.out.println("Vinegar and Oil Added");
+		
+	}
+
+}
